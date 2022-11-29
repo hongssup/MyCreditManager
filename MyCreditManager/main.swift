52c5f37758c527ce50cca1c1be1126a803bb7f94
hongssup <hongssup0202@gmail.com>
2022-11-29 18:49:12 +0900

    사전과제 제출

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,19 +7,23 @@
 
 import Foundation
 
+//입력받기
 let INPUT_MENU = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
-let INVALID_MENU = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
-let INVALID_INPUT = "입력이 잘못되었습니다. 다시 확인해주세요."
 let INPUT_NAME = "추가할 학생의 이름을 입력해주세요"
-let ADD_NAME_SUCCESS = " 학생을 추가했습니다."
-let ADD_NAME_FAIL = "은 이미 존재하는 학생입니다. 추가하지 않습니다."
 let INPUT_NAME_TO_DELETE = "삭제할 학생의 이름을 입력해주세요"
-let DELETE_NAME_SUCCESS = " 학생을 삭제하였습니다."
-let DELETE_NAME_FAIL = " 학생을 찾지 못했습니다."
 let INPUT_CREDIT = "성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Micky Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다."
 let INPUT_CREDIT_TO_DELETE = "성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Micky Swift"
-let INVALID_NAME = " 학생을 찾지 못했습니다."
 let INPUT_GET_CREDIT = "평점을 알고싶은 학생의 이름을 입력해주세요"
+//잘못된 입력
+let INVALID_MENU = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
+let INVALID_INPUT = "입력이 잘못되었습니다. 다시 확인해주세요."
+let INVALID_NAME = " 학생을 찾지 못했습니다."
+//추가 및 삭제
+let ADD_NAME_SUCCESS = " 학생을 추가했습니다."
+let ADD_NAME_FAIL = "은 이미 존재하는 학생입니다. 추가하지 않습니다."
+let DELETE_NAME_SUCCESS = " 학생을 삭제하였습니다."
+let DELETE_NAME_FAIL = " 학생을 찾지 못했습니다."
+//종료
 let EXIT = "프로그램을 종료합니다..."
 
 var runMyCreditManager = true
